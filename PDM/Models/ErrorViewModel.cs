5fd0c946b55dfc1df9fdd8a4984abd951ac96fab
BoksanD <danijel.boksan@gmail.com>
2017-09-19 13:16:14 +0200

    Add project files.

---
+++ PDM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PDM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
